d972983d7a5e1f198ba0fc3d791cb57db8ba59e8
Czarek <you@example.com>
2022-01-09 20:58:30 +0100

    39. Zadanie na 6 - zamiana @RestController na @Controller i @GetMapping na @RequestMapping

--- src/main/java/io/github/czarnyxtimon/controller/TaskController.java
+++ src/main/java/io/github/czarnyxtimon/controller/TaskController.java
@@ -6,13 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -21,25 +22,25 @@ class TaskController {
         this.repository = repository;
     }
 
-    @PostMapping("/tasks")
+    @RequestMapping(method = RequestMethod.POST,path ="/tasks")
     ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
+    @RequestMapping(method = RequestMethod.GET,path = "/tasks")
+    ResponseEntity<Task> readAllTasks(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,path = "/tasks", params = {"!sort","!page","!size"})
     ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository. findAll());
     }
 
-    @GetMapping(value = "/tasks")
-    ResponseEntity<List<Task>> readAllTasks(Pageable page){
-        logger.info("Custom pageable");
-        return ResponseEntity.ok(repository.findAll(page).getContent());
-    }
-
-    @GetMapping("/tasks/{id}")
+    @RequestMapping(method = RequestMethod.GET,path ="/tasks/{id}")
     ResponseEntity<Task> readTask(@PathVariable int id){
         return repository.findById(id)
                 .map(ResponseEntity::ok) //zapis method reference a pod spodem wersja lambda
@@ -48,7 +49,7 @@ class TaskController {
 //                .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/tasks/{id}")
+    @RequestMapping(method = RequestMethod.PUT,path ="/tasks/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
         if(!repository.existsById(id)){
             return ResponseEntity.notFound().build();
