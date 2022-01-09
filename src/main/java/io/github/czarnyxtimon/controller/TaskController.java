3cd5586f1f7fb2a9c509f4b5c2f9028f14aaf1fa
Czarek <you@example.com>
2022-01-09 19:29:56 +0100

    Zadanie 2: GET task, POST tasks

--- src/main/java/io/github/czarnyxtimon/controller/TaskController.java
+++ src/main/java/io/github/czarnyxtimon/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,6 +19,12 @@ class TaskController {
 
     TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
@@ -30,6 +37,15 @@ class TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        return repository.findById(id)
+                .map(ResponseEntity::ok) //zapis method reference a pod spodem wersja lambda
+                .orElse(ResponseEntity.notFound().build());
+//        return repository.findById(id).map(task -> ResponseEntity.ok(task))
+//                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/tasks/{id}")
