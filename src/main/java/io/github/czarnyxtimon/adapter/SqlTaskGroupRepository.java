3f7f59f454aff716329d9e2a6662bda67ffe8cbf
Czarek <you@example.com>
2022-01-23 17:24:12 +0100

    82. HQL (JPQL)

--- src/main/java/io/github/czarnyxtimon/adapter/SqlTaskGroupRepository.java
+++ src/main/java/io/github/czarnyxtimon/adapter/SqlTaskGroupRepository.java
@@ -3,8 +3,14 @@ package io.github.czarnyxtimon.adapter;
 import io.github.czarnyxtimon.model.TaskGroup;
 import io.github.czarnyxtimon.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
 }
