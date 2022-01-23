c9f0c68287bd55bb09e5ab66eeca978da63062ba
Czarek <you@example.com>
2022-01-23 21:26:14 +0100

    91. Serwis grup - toggle

--- src/main/java/io/github/czarnyxtimon/logic/TaskGroupService.java
+++ src/main/java/io/github/czarnyxtimon/logic/TaskGroupService.java
@@ -1,11 +1,11 @@
 package io.github.czarnyxtimon.logic;
 
-import io.github.czarnyxtimon.model.Task;
+import io.github.czarnyxtimon.TaskConfigurationProperties;
 import io.github.czarnyxtimon.model.TaskGroup;
 import io.github.czarnyxtimon.model.TaskGroupRepository;
+import io.github.czarnyxtimon.model.TaskRepository;
 import io.github.czarnyxtimon.model.projection.GroupReadModel;
 import io.github.czarnyxtimon.model.projection.GroupWriteModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +14,11 @@ import java.util.stream.Collectors;
 @Service
 public class TaskGroupService {
     private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
 
-    TaskGroupService(final TaskGroupRepository repository) {
+    TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
         this.repository = repository;
+        this.taskRepository = taskRepository;
     }
 
     public GroupReadModel createGroup(GroupWriteModel source) {
@@ -28,5 +30,14 @@ public class TaskGroupService {
         return repository.findAll().stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId) {
+        if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
+            throw new IllegalStateException("Group has indone tasks. Done all the tasks first");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given id not found"));
+        result.setDone(!result.isDone());
     }
 }
